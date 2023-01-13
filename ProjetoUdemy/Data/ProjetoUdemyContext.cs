14f6e9bee726420d8511df84afdb99525282ad87
Robson Alves Rocha <robson_it@outlook.com>
2023-01-13 14:36:26 -0300

    CRUD Scaffolding

---
+++ ProjetoUdemy/Data/ProjetoUdemyContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoUdemy.Models;
+
+namespace ProjetoUdemy.Data
+{
+    public class ProjetoUdemyContext : DbContext
+    {
+        public ProjetoUdemyContext (DbContextOptions<ProjetoUdemyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoUdemy.Models.Departamento> Departamento { get; set; } = default!;
+    }
+}
