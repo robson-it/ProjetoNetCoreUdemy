810bd1f07fefcccbe179fa05b52f2bb152d3c485
Robson Alves Rocha <robson_it@outlook.com>
2023-01-21 17:18:16 -0300

    PaginasBuscaSimplesBuscaAgrupada

---
+++ WebVendas/Controllers/RegistroVendasController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebVendas.Controllers
+{
+    public class RegistroVendasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BuscaSimples()
+        {
+            return View();
+        }
+
+        public IActionResult BuscaAgrupada()
+        {
+            return View();
+        }
+
+
+    }
+}
